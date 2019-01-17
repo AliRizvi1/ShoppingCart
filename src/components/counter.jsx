5c3b424c27bf095761c071f29551f14bc3f4a77a
Ali <ali.rizvi1@gmail.com>
2019-01-17 16:13:06 -0500

    Add more components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 class Counter extends Component {
 	//State is an object that includes any data that the component needs
 	state = {
-		count: 1
+		count: 0,
 	};
 	render() {
 		return (
 			<React.Fragment>
-				<span>{this.state.count}</span>
+				<span>className=""{this.formatCount()}</span>
 				<button>Increment</button>
 			</React.Fragment>
 			);
 	}
+
+	formatCount() {
+		const { count } = this.state;
+
+		return count === 0 ? <h1>Zero</h1> : count;
+	} 
 }
 
 export default Counter;
