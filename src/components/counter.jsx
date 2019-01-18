5c08ce7cb1d1692b4776d2cee666f3ac996b09e2
Ali <ali.rizvi1@gmail.com>
2019-01-18 18:29:14 -0500

    Counter messages

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,8 +15,10 @@ class Counter extends Component {
 	}
 
 	formatCount() {
-		const { count } = this.state;
 
+		const { count } = this.state;
+		//Counter that outputs Zero spelled out when the count is zero, otherwise it will output the number
+		//Javascript uses === for boolean expressions
 		return count === 0 ? <h1>Zero</h1> : count;
 	} 
 }
