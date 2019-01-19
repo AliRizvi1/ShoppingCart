ed1f11e937e8e1388a1f8e428cd6e7dc43f12332
Ali <ali.rizvi1@gmail.com>
2019-01-19 16:16:51 -0500

    Increment handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,8 +4,20 @@ class Counter extends Component {
 	//State is an object that includes any data that the component needs
 	state = {
 		count: 0 ,
-		tags: []
+		tags: ["tag1","tag2"]
 	};
+
+	// constructor() {
+	// 	super();
+	// 	//Using a javascript bind event handler
+	// 	this.handleIncrement = this.handleIncrement.bind(this);
+	// }
+
+	//Can set to arrow function, much more simple than the binding constructor above
+	handleIncrement = () => {
+		//Can't modify state directly in React, have to let React know that the state is changing
+		this.setState({count : this.state.count + 1});
+	}
 
 	renderTags() {
 		if (this.state.tags.length === 0) return  <p>There are no tags!</p>;
@@ -17,12 +29,34 @@ class Counter extends Component {
 		return (
 			//Essentially created an if statement below
 			//.jsx doesn't support if statements
+			//a non-empty string is considered "truthy" by JavaScript
 			<div>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button 
+					onClick={this.handleIncrement} 
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
 				{this.state.tags.length === 0 && "Please create a new tag!"}
 				{this.renderTags()}
+
 				
 			</div>
 			);
+	}
+
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? <h1>Zero</h1> : count;
+
 	}
 }
 
