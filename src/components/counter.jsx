80a9f0e8181563feffbf6033c5cb6e72efd51498
Ali <ali.rizvi1@gmail.com>
2019-01-19 14:54:13 -0500

    Dynamically rendered lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,29 +3,27 @@ import React, { Component } from 'react';
 class Counter extends Component {
 	//State is an object that includes any data that the component needs
 	state = {
-		count: 0,
+		count: 0 ,
+		tags: []
 	};
 
-	styles = {
-		fontSize: 5,
-		fontWeight: "bold"
-	};
-	render() {
-		return (
-			<React.Fragment>
-				<span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
-			</React.Fragment>
-			);
+	renderTags() {
+		if (this.state.tags.length === 0) return  <p>There are no tags!</p>;
+
+		return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
 	}
 
-	formatCount() {
-
-		const { count } = this.state;
-		//Counter that outputs Zero spelled out when the count is zero, otherwise it will output the number
-		//Javascript uses === for boolean expressions
-		return count === 0 ? <h1>Zero</h1> : count;
-	} 
+	render() {
+		return (
+			//Essentially created an if statement below
+			//.jsx doesn't support if statements
+			<div>
+				{this.state.tags.length === 0 && "Please create a new tag!"}
+				{this.renderTags()}
+				
+			</div>
+			);
+	}
 }
 
 export default Counter;
